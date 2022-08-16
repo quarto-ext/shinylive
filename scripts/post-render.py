3c69bbba0ea1ba6775e6a23e19368cf217841d17
Winston Chang <winston@stdout.org>
2022-08-16 16:32:55 -0500

    Code cleanup

--- scripts/post-render.py
+++ scripts/post-render.py
@@ -4,16 +4,16 @@ import shutil
 import os
 import shiny
 
-# Directory with built shinylive assets
-# This function will download and untar the assets if necessary, and then return
-# the path to the assets directory.
-SHINYLIVE_DIR = shiny._static._ensure_shinylive_local()
-
 # Continue past this part only if building entire site.
 if not os.getenv("QUARTO_PROJECT_RENDER_ALL"):
     exit()
 
 open("docs/.nojekyll", "a").close()
+
+# Directory with built shinylive assets
+# This function will download and untar the assets if necessary, and then return
+# the path to the assets directory.
+SHINYLIVE_DIR = shiny._static._ensure_shinylive_local()
 
 # It would be more convenient to copy these files using `resources` in
 # _quarto.yml, but it doesn't seem to allow choosing the destination directory,
