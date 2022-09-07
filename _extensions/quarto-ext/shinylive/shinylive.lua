7db66e7381d82aee8578d45649fc66a1796bde77
Winston Chang <winston@stdout.org>
2022-09-06 19:39:42 -0500

    Code cleanup and add CSS

--- _extensions/quarto-ext/shinylive/shinylive.lua
+++ _extensions/quarto-ext/shinylive/shinylive.lua
@@ -1,40 +1,41 @@
-local needsShinylive = false
 local codeblockScript = nil
-local baseShinyliveHtmlDeps = nil
 
 function getShinyliveBaseDeps()
+  -- Relative path from the current page to the root of the site. This is needed
+  -- to find out where shinylive-sw.js is, relative to the current page.
   local projectOffset = param("project-offset")
-
-  local dep_json = pandoc.pipe("shinylive", { "base-deps", "--sw-dir", projectOffset}, "")
-  local deps = quarto.json.decode(dep_json)
-
+  local depJson = pandoc.pipe("shinylive", { "base-deps", "--sw-dir", projectOffset}, "")
+  local deps = quarto.json.decode(depJson)
   return deps
 end
 
---------------------------------------------------------------------------------
 
 return {
   {
     Pandoc = function (doc)
+      -- Find the path to codeblock-to-json.ts and save it for later use.
       codeblockScript = pandoc.pipe("shinylive", { "codeblock-to-json-path" }, "")
       -- Remove trailing whitespace
       codeblockScript = codeblockScript:gsub("%s+$", "")
 
       local baseDeps = getShinyliveBaseDeps()
-
-      quarto.utils.dump(baseDeps)
       for idx, dep in ipairs(baseDeps) do
         quarto.doc.addHtmlDependency(dep)
       end
+
+      quarto.doc.addHtmlDependency(
+        {
+          name = "shinylive-quarto-css",
+          stylesheets = {"resources/css/shinylive-quarto.css"}
+        }
+      )
 
       return doc
     end
   },
   {
     CodeBlock = function(el)
-      if el.attr and el.attr.classes:includes("{shinyapp-py}") then
-        needsShinylive = true
-
+      if el.attr and el.attr.classes:includes("{shinyapp-python}") then
         -- Convert code block to JSON string in the same format as app.json.
         local appJson = pandoc.pipe(
           "deno",
