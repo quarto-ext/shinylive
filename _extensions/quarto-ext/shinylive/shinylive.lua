a46da6062af8ef1fe0044dcebb8467c6817d16ff
Barret Schloerke <barret@posit.co>
2023-09-08 17:21:07 -0400

    Support posit-dev/r-shinylive

--- _extensions/quarto-ext/shinylive/shinylive.lua
+++ _extensions/quarto-ext/shinylive/shinylive.lua
@@ -2,8 +2,8 @@ local hasDoneShinyliveSetup = false
 local codeblockScript = nil
 
 -- Try calling `pandoc.pipe('shinylive', ...)` and if it fails, print a message
--- about installing shinylive package.
-function callShinylive(args, input)
+-- about installing shinylive python package.
+function callPythonShinylive(args, input)
   local res
   local status, err = pcall(
     function()
@@ -19,20 +19,54 @@ function callShinylive(args, input)
   return res
 end
 
+-- Try calling `pandoc.pipe('Rscript', ...)` and if it fails, print a message
+-- about installing shinylive R package.
+function callRShinylive(args, input)
+  args = { "-e",
+    -- TODO-barret; Remove load_all()
+    -- "shinylive::quarto_ext()",
+    "pkgload::load_all('../../', quiet = TRUE); shinylive::quarto_ext()",
+    table.unpack(args) }
+  local res
+  local status, err = pcall(
+    function()
+      res = pandoc.pipe("Rscript", args, input)
+    end
+  )
+
+  if not status then
+    print(err)
+    error(
+      "Error running 'Rscript' command. Perhaps you need to install the 'shinylive' R package?")
+  end
+
+  return res
+end
+
+function callShinylive(language, args, input)
+  -- print("Calling " .. language .. " shinylive with args: " .. table.concat(args, " "))
+  if language == "python" then
+    return callPythonShinylive(args, input)
+  elseif language == "r" then
+    return callRShinylive(args, input)
+  else
+    error("Unknown language: " .. language)
+  end
+end
 
 -- Do one-time setup when a Shinylive codeblock is encountered.
-function ensureShinyliveSetup()
+function ensureShinyliveSetup(language)
   if hasDoneShinyliveSetup then
     return
   end
   hasDoneShinyliveSetup = true
 
   -- Find the path to codeblock-to-json.ts and save it for later use.
-  codeblockScript = callShinylive({ "codeblock-to-json-path" }, "")
+  codeblockScript = callShinylive(language, { "codeblock-to-json-path" }, "")
   -- Remove trailing whitespace
   codeblockScript = codeblockScript:gsub("%s+$", "")
 
-  local baseDeps = getShinyliveBaseDeps()
+  local baseDeps = getShinyliveBaseDeps(language)
   for idx, dep in ipairs(baseDeps) do
     quarto.doc.add_html_dependency(dep)
   end
@@ -40,19 +74,19 @@ function ensureShinyliveSetup()
   quarto.doc.add_html_dependency(
     {
       name = "shinylive-quarto-css",
-      stylesheets = {"resources/css/shinylive-quarto.css"}
+      stylesheets = { "resources/css/shinylive-quarto.css" }
     }
   )
 end
 
-
-function getShinyliveBaseDeps()
+function getShinyliveBaseDeps(language)
   -- Relative path from the current page to the root of the site. This is needed
   -- to find out where shinylive-sw.js is, relative to the current page.
   if quarto.project.offset == nil then
     error("The shinylive extension must be used in a Quarto project directory (with a _quarto.yml file).")
   end
   local depJson = callShinylive(
+    language,
     { "base-deps", "--sw-dir", quarto.project.offset },
     ""
   )
@@ -61,15 +95,18 @@ function getShinyliveBaseDeps()
   return deps
 end
 
-
 return {
   {
     CodeBlock = function(el)
       if el.attr and (
-        el.attr.classes:includes("{shinylive-python}")
-        or el.attr.classes:includes("{shinylive-r}")
-     ) then
-        ensureShinyliveSetup()
+            el.attr.classes:includes("{shinylive-python}")
+            or el.attr.classes:includes("{shinylive-r}")
+          ) then
+        local language = "python"
+        if el.attr.classes:includes("{shinylive-r}") then
+          language = "r"
+        end
+        ensureShinyliveSetup(language)
 
         -- Convert code block to JSON string in the same format as app.json.
         local parsedCodeblockJson = pandoc.pipe(
@@ -83,6 +120,7 @@ return {
 
         -- Find Python package dependencies for the current app.
         local appDepsJson = callShinylive(
+          language,
           { "package-deps" },
           quarto.json.encode(parsedCodeblock["files"])
         )
