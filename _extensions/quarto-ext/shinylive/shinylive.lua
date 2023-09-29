ac50f67e8ddb056ffabaeafa0ff1f437c3737507
Barret Schloerke <barret@posit.co>
2023-09-29 13:07:44 -0400

    Document

--- _extensions/quarto-ext/shinylive/shinylive.lua
+++ _extensions/quarto-ext/shinylive/shinylive.lua
@@ -1,12 +1,123 @@
-local hasDoneShinyliveSetup = false
+-- # Shinylive package methods
+-- Always use `callShinyLive()` to call the shinylive extension.
+-- `callPythonShinyLive()` and `callRShinyLive()` should not be used directly. Instead, always use `callShinyLive()`.
+
+-- # py-shinylive and r-shinylive methods
+-- info               - Package, version, asset version, and script paths information
+-- base-htmldeps      - Quarto html dependencies for the base shinylive integration
+-- language-resources - R's resource files for the quarto html dependency named `shinylive`
+-- app-resources      - App-specific resource files for the quarto html dependency named `shinylive`
+
+-- ### CLI Interface
+-- * `extension info`
+--   * Prints information about the extension including:
+--     * `version`: The version of the R package
+--     * `assets_version`: The version of the web assets
+--     * `scripts`: A list of paths scripts that are used by the extension,
+--      mainly `codeblock-to-json`
+--   * Example
+--     ```
+--     {
+--       "version": "0.1.0",
+--       "assets_version": "0.2.0",
+--       "scripts": {
+--         "codeblock-to-json": "/<ASSETS_CACHE_DIR>/shinylive-0.2.0/scripts/codeblock-to-json.js"
+--       }
+--     }
+--     ```
+-- * `extension base-htmldeps`
+--   * Prints the language agnostic quarto html dependencies as a JSON array.
+--     * The first html dependency is the `shinylive` service workers.
+--     * The second html dependency is the `shinylive` base dependencies. This
+--       dependency will contain the core `shinylive` asset scripts (JS files
+--       automatically sourced), stylesheets (CSS files that are automatically
+--       included), and resources (additional files that the JS and CSS files can
+--       source).
+--   * Example
+--     ```
+--     [
+--       {
+--         "name": "shinylive-serviceworker",
+--         "version": "0.2.0",
+--         "meta": { "shinylive:serviceworker_dir": "." },
+--         "serviceworkers": [
+--           {
+--             "source": "/<ASSETS_CACHE_DIR>/shinylive-0.2.0/shinylive-sw.js",
+--             "destination": "/shinylive-sw.js"
+--           }
+--         ]
+--       },
+--       {
+--         "name": "shinylive",
+--         "version": "0.2.0",
+--         "scripts": [{
+--           "name": "shinylive/load-shinylive-sw.js",
+--           "path": "/<ASSETS_CACHE_DIR>/shinylive-0.2.0/shinylive/load-shinylive-sw.js",
+--             "attribs": { "type": "module" }
+--         }],
+--         "stylesheets": [{
+--           "name": "shinylive/shinylive.css",
+--           "path": "/<ASSETS_CACHE_DIR>/shinylive-0.2.0/shinylive/shinylive.css"
+--         }],
+--         "resources": [
+--           {
+--             "name": "shinylive/shinylive.js",
+--             "path": "/<ASSETS_CACHE_DIR>/shinylive-0.2.0/shinylive/shinylive.js"
+--           },
+--           ... # [ truncated ]
+--         ]
+--       }
+--     ]
+--     ```
+-- * `extension language-resources`
+--   * Prints the language-specific resource files as JSON that should be added to the quarto html dependency.
+--     * For r-shinylive, this includes the webr resource files
+--     * For py-shinylive, this includes the pyodide and pyright resource files.
+--   * Example
+--     ```
+--     [
+--       {
+--         "name": "shinylive/webr/esbuild.d.ts",
+--         "path": "/<ASSETS_CACHE_DIR>/shinylive-0.2.0/shinylive/webr/esbuild.d.ts"
+--       },
+--       {
+--         "name": "shinylive/webr/libRblas.so",
+--         "path": "/<ASSETS_CACHE_DIR>/shinylive-0.2.0/shinylive/webr/libRblas.so"
+--       },
+--       ... # [ truncated ]
+--     ]
+-- * `extension app-resources`
+--   * Prints app-specific resource files as JSON that should be added to the `"shinylive"` quarto html dependency.
+--   * Currently, r-shinylive does not return any resource files.
+--   * Example
+--     ```
+--     [
+--       {
+--         "name": "shinylive/pyodide/anyio-3.7.0-py3-none-any.whl",
+--         "path": "/<ASSETS_CACHE_DIR>/shinylive-0.2.0/shinylive/pyodide/anyio-3.7.0-py3-none-any.whl"
+--       },
+--       {
+--         "name": "shinylive/pyodide/appdirs-1.4.4-py2.py3-none-any.whl",
+--         "path": "/<ASSETS_CACHE_DIR>/shinylive-0.2.0/shinylive/pyodide/appdirs-1.4.4-py2.py3-none-any.whl"
+--       },
+--       ... # [ truncated ]
+--     ]
+--     ```
+
+
+
+
 local hasDoneSetup = { init = false, r = false, python = false }
 local versions = { r = nil, python = nil }
 local codeblockScript = nil
 local appSpecificDeps = {}
 
--- Try calling `pandoc.pipe('shinylive', ...)` and if it fails, print a message
--- about installing shinylive python package.
+-- Python specific method to call py-shinylive
+-- @param args: list of string arguments to pass to py-shinylive
+-- @param input: string to pipe into to py-shinylive
 function callPythonShinylive(args, input)
+  -- Try calling `pandoc.pipe('shinylive', ...)` and if it fails, print a message
+  -- about installing shinylive python package.
   local res
   local status, err = pcall(
     function()
@@ -22,12 +133,16 @@ function callPythonShinylive(args, input)
   return res
 end
 
--- Try calling `pandoc.pipe('Rscript', ...)` and if it fails, print a message
--- about installing shinylive R package.
+-- R specific method to call {r-shinylive}
+-- @param args: list of string arguments to pass to r-shinylive
+-- @param input: string to pipe into to r-shinylive
 function callRShinylive(args, input)
   args = { "-e",
     "shinylive:::quarto_ext()",
     table.unpack(args) }
+
+  -- Try calling `pandoc.pipe('Rscript', ...)` and if it fails, print a message
+  -- about installing shinylive R package.
   local res
   local status, err = pcall(
     function()
@@ -44,6 +159,9 @@ function callRShinylive(args, input)
   return res
 end
 
+-- Returns decoded object
+-- @param language: "python" or "r"
+-- @param args, input: see `callPythonShinylive` and `callRShinylive`
 function callShinylive(language, args, input)
   if input == nil then
     input = ""
@@ -112,6 +230,9 @@ function ensureInitSetup(language)
   )
 end
 
+-- Do one-time setup for language specific html dependencies.
+-- This should only be called once per document
+-- @param language: "python" or "r"
 function ensureLanguageSetup(language)
   ensureInitSetup(language)
 
