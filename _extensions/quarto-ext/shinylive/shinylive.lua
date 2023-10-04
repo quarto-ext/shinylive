8b2598e51b8b3e67c0a7a52d525d78adaaef9ed5
Barret Schloerke <barret@posit.co>
2023-10-04 15:50:23 -0400

    Add minimum python shinylive version support. Add matching r/python shinylive assets support

--- _extensions/quarto-ext/shinylive/shinylive.lua
+++ _extensions/quarto-ext/shinylive/shinylive.lua
@@ -73,10 +73,14 @@ end
 -- Returns decoded object
 -- @param language: "python" or "r"
 -- @param args, input: see `callPythonShinylive` and `callRShinylive`
-function callShinylive(language, args, input)
+function callShinylive(language, args, input, parseJson)
   if input == nil then
     input = ""
   end
+  if parseJson == nil then
+    parseJson = true
+  end
+
   local res
   -- print("Calling " .. language .. " shinylive with args: " .. table.concat(args, " "))
   if language == "python" then
@@ -85,6 +89,10 @@ function callShinylive(language, args, input)
     res = callRShinylive(args, input)
   else
     assert(false, "Unknown language: " .. language)
+  end
+
+  if not parseJson then
+    return res
   end
 
   -- Remove any unwanted output before the first curly brace or square bracket.
@@ -113,6 +121,7 @@ function callShinylive(language, args, input)
     res = string.sub(res, min_start)
   end
 
+
   -- Decode JSON object
   local result
   local status, err = pcall(
@@ -128,6 +137,109 @@ function callShinylive(language, args, input)
     assert(false, "Error decoding JSON response from `shinylive` " .. language .. " package.")
   end
   return result
+end
+
+function parseVersion(versionTxt)
+  local versionParts = {}
+  for part in string.gmatch(versionTxt, "%d+") do
+    table.insert(versionParts, tonumber(part))
+  end
+  local ret = {
+    major = nil,
+    minor = nil,
+    patch = nil,
+    extra = nil,
+    length = #versionParts,
+    str = versionTxt
+  }
+
+  if ret.length >= 1 then
+    ret.major = versionParts[1]
+    if ret.length >= 2 then
+      ret.minor = versionParts[2]
+      if ret.length >= 3 then
+        ret.patch = versionParts[3]
+        if ret.length >= 4 then
+          ret.extra = versionParts[4]
+        end
+      end
+    end
+  end
+
+  return ret
+end
+
+-- If verA > verB, return 1
+-- If verA == verB, return 0
+-- If verA < verB, return -1
+function compareVersions(verA, verB)
+  if verA.major == nil or verB.major == nil then
+    assert(false, "Invalid version: " .. verA.str .. " or " .. verB.str)
+  end
+
+  for index, key in ipairs({ "major", "minor", "patch", "extra" }) do
+    local partDiff = compareVersionPart(verA[key], verB[key])
+    if partDiff ~= 0 then
+      return partDiff
+    end
+  end
+
+  -- Equal!
+  return 0
+end
+
+function compareVersionPart(aPart, bPart)
+  if aPart == nil and bPart == nil then
+    return 0
+  end
+  if aPart == nil then
+    return -1
+  end
+  if bPart == nil then
+    return 1
+  end
+  if aPart > bPart then
+    return 1
+  elseif aPart < bPart then
+    return -1
+  end
+
+  -- Equal!
+  return 0
+end
+
+function ensurePyshinyliveVersion(language)
+  -- Quit early if not python
+  if language ~= "python" then
+    return
+  end
+  -- Quit early if already completed check
+  if hasDoneSetup.python_version then
+    return
+  end
+  hasDoneSetup.python_version = true
+
+  -- Verify that min python shinylive version is met
+  pyShinyliveVersion = callShinylive(language, { "--version" }, "", false)
+  -- Remove trailing whitespace
+  pyShinyliveVersion = pyShinyliveVersion:gsub("%s+$", "")
+  -- Parse version into table
+  parsedVersion = parseVersion(pyShinyliveVersion)
+
+  -- Verify that the version is at least 0.1.0
+  if
+      (parsedVersion.length < 3) or
+      -- Major and minor values are 0. Ex: 0.0.18
+      (parsedVersion.major == 0 and parsedVersion.minor == 0)
+  then
+    assert(nil,
+      "\nThe shinylive Python package must be at least version v0.1.0 to be used in a quarto document." ..
+      "\n\nInstalled Python Shinylive package version: " .. pyShinyliveVersion ..
+      "\n\nPlease upgrade the Python Shinylive package by running:" ..
+      "\n\tpip install --upgrade shinylive" ..
+      "\n\n(If you are using a virtual environment, please activate it before running the command above.)"
+    )
+  end
 end
 
 -- Do one-time setup for language agnostic html dependencies.
@@ -166,7 +278,11 @@ end
 -- This should only be called once per document
 -- @param language: "python" or "r"
 function ensureLanguageSetup(language)
-  ensureInitSetup(language)
+  -- Min version check must be done first
+  ensurePyshinyliveVersion(language)
+
+  -- Make sure the base setup is done before the langage setup
+  ensureBaseSetup(language)
 
   if hasDoneSetup[language] then
     return
@@ -184,18 +300,41 @@ function ensureLanguageSetup(language)
       ---@diagnostic disable-next-line: undefined-field
       versions.r.assets_version ~= versions.python.assets_version
   then
-    error(
-      "The shinylive R and Python packages must support the same Shinylive Assets version to be used in the same quarto document." ..
-      "\nR" ..
+    local parsedRAssetsVersion = parseVersion(versions.r.assets_version)
+    local parsedPythonAssetsVersion = parseVersion(versions.python.assets_version)
+
+    local verDiff = compareVersions(parsedRAssetsVersion, parsedPythonAssetsVersion)
+    local verDiffStr = ""
+    if verDiff == 1 then
+      -- R shinylive supports higher version of assets. Upgrade python shinylive
+      verDiffStr =
+          "The currently installed python shinylive package supports a lower assets version, " ..
+          "therefore we recommend updating your python shinylive package to the latest version."
+    elseif verDiff == -1 then
+      -- Python shinylive supports higher version of assets. Upgrade R shinylive
+      verDiffStr =
+          "The currently installed R shinylive package supports a lower assets version, " ..
+          "therefore we recommend updating your R shinylive package to the latest version."
+    end
+
+    assert(false,
+      "\nThe shinylive R and Python packages must support the same Shinylive Assets version to be used in the same quarto document." ..
+      "\n" ..
+      "\nPython shinylive package version: " ..
       ---@diagnostic disable-next-line: undefined-field
-      "\n\tShinylive package version: " .. versions.r.version ..
+      versions.python.version .. " ; Supported assets version: " .. versions.python.assets_version ..
+      "\nR shinylive package version:      " ..
       ---@diagnostic disable-next-line: undefined-field
-      "\n\tSupported ssets version: " .. versions.r.assets_version ..
-      "\nPython" ..
-      ---@diagnostic disable-next-line: undefined-field
-      "\n\tShinylive package version: " .. versions.python.version ..
-      ---@diagnostic disable-next-line: undefined-field
-      "\n\tSupported ssets version: " .. versions.python.assets_version
+      versions.r.version .. " ; Supported assets version: " .. versions.r.assets_version ..
+      "\n" ..
+      "\n" .. verDiffStr ..
+      "\n" ..
+      "\nTo update your R Shinylive package, run:" ..
+      "\n\tR -e \"install.packages('shinylive')\"" ..
+      "\n" ..
+      "\nTo update your Python Shinylive package, run:" ..
+      "\n\tpip install --upgrade shinylive" ..
+      "\n(If you are using a virtual environment, please activate it before running the command above.)"
     )
   end
 
